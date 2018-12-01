5570287f2e396d0eda5cec50e03fcb53d8c50f9f
talfeust <thalfeust@gmail.com>
2018-12-01 11:41:57 +0100

    Path changement

--- robot.py
+++ robot.py
@@ -45,7 +45,7 @@ def is_black(value):
 def is_light_black(value):
 	return value<500
 
-#{{{ Logger class 
+#{{{ Logger class
 class Logger:
 	def __init__(self):
 		self.file = open(LOG_FILE, "at")
@@ -53,7 +53,7 @@ class Logger:
 
 	def __del__(self):
 		self.file.close()
-	
+
 	def log(self, text, level=0):
 		print(text)
 		self.file.write(text+"\n")
@@ -78,14 +78,14 @@ class Robot:
 
 		signal.signal(signal.SIGINT, self.exit_gracefully)
 		signal.signal(signal.SIGTERM, self.exit_gracefully)
-		
+
 		self.leftMotor.reset()
 		self.rightMotor.reset()
 		self.running = True
 		self.leftMotor.run_direct()
 		self.rightMotor.run_direct()
 
-	
+
 	def shutdown(self):
 		self.rightMotor.duty_cycle_sp = 0
 		self.leftMotor.duty_cycle_sp = 0
@@ -120,20 +120,20 @@ class Robot:
 				else:
 					self.leftMotor.duty_cycle_sp = BASE_SPEED
 					self.rightMotor.duty_cycle_sp = BASE_SPEED
-	
+
 	def run_backward(self):
 		self.rightMotor.duty_cycle_sp = BASE_SPEED
 		self.leftMotor.duty_cycle_sp = BASE_SPEED
 		self.rightMotor.polarity = "inversed"
 		self.leftMotor.polarity = "inversed"
-		
-		
+
+
 		while self.running:
 			left = self.leftSensor.value()
 			right = self.rightSensor.value()
 			if PRINT: print("{} {}".format(left, right))
 			if is_black(left) and is_black(right):
-				
+
 				while self.running:
 					left = self.leftSensor.value()
 					right = self.rightSensor.value()
@@ -186,7 +186,7 @@ class Robot:
 					self.rightMotor.duty_cycle_sp = CORRECTION_SPEED_INSIDE
 				else:
 					self.leftMotor.duty_cycle_sp = BASE_SPEED-10
-					self.rightMotor.duty_cycle_sp = BASE_SPEED-10	
+					self.rightMotor.duty_cycle_sp = BASE_SPEED-10
 	def escape_from_black(self):
 		self.leftMotor.duty_cycle_sp = TURN_SPEED_90
 		self.rightMotor.duty_cycle_sp = TURN_SPEED_90
@@ -199,7 +199,7 @@ class Robot:
 			if not is_black(right) or not is_black(left):
 				self.stop()
 				return
-			
+
 	def turn_right(self):
 		last_color = self.rightSensor.value()
 		self.leftMotor.duty_cycle_sp = TURN_SPEED_90
@@ -258,7 +258,7 @@ class Robot:
 						self.stop()
 						self.run_forward()
 						return
-			
+
 			last_color = new_color
 
 	def turn_left_left(self, is_between_cross = False):
@@ -287,7 +287,7 @@ class Robot:
 						self.stop()
 						self.run_forward()
 						return
-			
+
 			last_color = new_color
 
 	def stop(self):
@@ -347,7 +347,7 @@ class InstructionConverter:
 			x= a+'l'+b
 		self.orientation = new
 		return x
-	
+
 	def switch_direction(direction):
 		if direction == 'n' or direction == 'N':
 			return 0
@@ -359,7 +359,7 @@ class InstructionConverter:
 			return 3
 		LOGGER.log("Error switch_direction")
 		raise Exception()
-	
+
 	def translate(self,instruction):
 		self.has_can = False
 		self.previous = None
@@ -370,7 +370,7 @@ class InstructionConverter:
 			result = result + v
 			self.previous = i
 		return list(result)
-	
+
 	def convert_letter(i):
 		dict = {'u':'n', 'U':'N', 'd':'s', 'D':'S', 'l':'w', 'L':'W', 'r':'e', 'R':'E'}
 		res = ''
@@ -389,12 +389,14 @@ if __name__ == "__main__":
 	#instruction = ['f', 'r',  'c', 'b', 'a']
 	#robot.execution(instruction)
 	instruction = "UddlluuRRddlUrrruuuulldDDuuurrddddlLuuuuruulllddRRddddrruuuLUUUruLLLulDrrrddddrdddlluuUruuullddRdrUUUruLulDrddddrddLdlUUUruuuulldddRdrUUUddlluuurRurDlddddlddddlluRdrUUUUruuuulldddRdrUUUUddddldddlluRdrUUU"
-	
+
 	instruction = "eNsswwnnEEsW"
 	instruction = "d"
 	instruction = "dlllluuuuRRdrUUUruLLLulDrrrdddlllddrUluRRdrUUUruLdlUruLLrrddddlllddddrrrruLdllUUUluRRdrUUUluRurDlddddlldddrruLdlUUUluRRdrUUUluurrdLulD"
+	instruction = "ldllluuuuRRdrUUUruLLLulDrrrdddlllddrUluRRdrUUUUruLLLrrddddlllddddrUUUluRRdrUUUruLurDrdddrrddddrruLdlUUUluRRdrUUUruulldRurD"
+	ruLdllUUUluRRdrUUUluRurDlddddlldddrruLdlUUUluRRdrUUUluurrdLulD"
 	instruction = InstructionConverter.convert_letter(instruction)
-	instruction = list(instruction)	
+	instruction = list(instruction)
 	converter = InstructionConverter()
 	x=converter.translate(instruction)
 	#x = ['r']
